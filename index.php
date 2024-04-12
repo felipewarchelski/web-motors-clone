b6c826ea7d7600033c7ea8676a0b9db9bbc7cccd
Felipe Warchelski <fewarchelski@gmail.com>
2024-04-12 20:34:34 -0300

    Update

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="index.css">
     <title>Web Motors</title>
 </head>
+<!-- isLogged() ? echo $info['name'] : echo "Entrar" -->
 
 <body>
     <section class="header">
@@ -26,7 +27,19 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                </svg>Entrar
+                </svg>
+                <?php
+                require 'vendor/autoload.php';
+                use \App\Session\User as SessionUser;
+                $info = SessionUser::isLogged();
+
+                if(SessionUser::isLogged()) {
+                    $info = SessionUser::login($name, $login, $picture);
+                    echo $info['name'];
+                } else {
+                    echo "Entrar";
+                }
+                ?>
             </a>
             <a href="">
                 <img src="imgs/coracao.png" alt="" width="20px">
