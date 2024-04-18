2a8ca5e5dd4aeb153aff996b5ceffcd776dc3754
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-04-18 20:34:16 -0300

    .

--- app/includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    $con = mysqli_connect('localhost:3500', 'root', '');
-    $database = mysqli_select_db($con,'web_motors_project');
-
-    if (!$con || !$database) echo mysqli_error($con);
-
-?>
