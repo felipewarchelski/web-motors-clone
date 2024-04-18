6aa4910f93d0bdf1685a8855b44ac844b8cb3e32
Andre Gonçalves <dregoncalves22@gmail.com>
2024-04-18 20:42:06 -0300

    Página de perfil do usuário

--- public/perfil.php
+++ public/perfil.php
@@ -1,12 +1,43 @@
-<?php
- include '../app/includes/logout.php';
-?>
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb">
+    <link rel="stylesheet" href="styles/perfil.css">
     <title>Web Motors</title>
-    <link rel="icon" type="image/x-icon" href="../imgs/favicon.ico">
 </head>
-
+<body>
+    <section class="header">
+        <div class="logo">
+            <a href="index.php"><img src="../imgs/webmotors-logo-8.png" alt=""></a>
+        </div>
+        <div class="title">
+            <h2>Minha Garagem</h2>
+            <a href="#">
+                <img src="../imgs/chatting.png" alt="Chat">
+            </a>
+        </div>
+        <a class="user" href="#">
+            <div class="circle"> <?php //código pra puxar a primeira letra do usuário ?> </div>
+        </a>
+    </section>
+    <section class="main">
+        <div class="navbar">
+            <div class="navbar-header">
+                <div class="user-icon"></div>
+                <div class="user-data">
+                    <h3>nome</h3>
+                    <h4>email</h3>
+                </div>
+            </div>
+            <div class="nav-content">
+            <a href="#">Buscar veículo</a>
+            </div>
+        </div>
+        <div class="main-content">
+            <h1>Minha Conta</h1>
+        </div>
+    </section>
+</body>
 </html>
