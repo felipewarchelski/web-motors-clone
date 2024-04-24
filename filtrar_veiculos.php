efa1fc096ce9bc0c749ef7c101abec9e11295d5a
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-04-23 21:07:39 -0300

    Create filtrar_veiculos.php

---
+++ filtrar_veiculos.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'config.php';
+
+$colunas = ['marca', 'modelo', 'ano_lancamento', 'ano_fabricacao', 'cor', 'blindado'];
+$pesquisa = [$marca, $modelo, $ano_lancamento, $ano_fabricacao, $cor, $blindado];
+
+$condicoes = '';
+
+foreach ($colunas as $index => $coluna) {
+    $valor = $pesquisa[$index];
+
+    if ($valor !== null && $valor !== '') {
+        $valor = addslashes($valor); 
+        $condicoes .= "$coluna LIKE '%$valor%' AND ";
+    }
+}
+
+$condicoes = rtrim($condicoes, ' AND ');
+
+$query = "SELECT * FROM anuncio WHERE $condicoes;";
+
+
+    $result = mysqli_query($con, $query);
+
+    if ($result) {
+        $tableData = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $tableData[] = $row;
+        }
+    } else {
+        echo "Sem resultados para essa consulta! ";
+    }
+?>
